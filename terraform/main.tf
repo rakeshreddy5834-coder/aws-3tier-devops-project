de28cf908991a7eba9700436271caf80b8c798be
rakeshreddy5834-coder <rakeshreddy5834@gmail.com>
2026-03-28 13:19:53 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,13 @@ resource "aws_instance" "web" {
     Name = "DevOps-Project"
   }
 }
+resource "aws_security_group" "sg" {
+  name = "devops-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
